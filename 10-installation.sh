2958c6229eecb8a0a54a0aa0db66a9183e669e6d
prabhaks7 <sakereprabhakar@gmail.com>
2024-04-21 21:22:38 +0530

    hello script

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,8 +5,10 @@ USERID=$(id -u)
 if [ $ISERID -ne 0 ]
 then
   echo "Please run this script with root aaccess"
+  exit 1  # manually exit if error comes.
 else
   echo "You are super user." 
 fi
 
 dnf install mysql -y
+
