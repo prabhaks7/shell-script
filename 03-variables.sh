4db49731306598933c1cdd20e0d9533729d6d3c2
prabhaks7 <sakereprabhakar@gmail.com>
2024-04-21 18:11:47 +0530

    hello script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Prabha
-PERSON2=Ketan
+PERSON1=Rahul
+PERSON2=Mayur
 
 echo "$PERSON1: : Hello $PERSON2, How are you?"
 echo "$PERSON2: : Hi $PERSON1, I'm good, thanks for asking. How are you"
